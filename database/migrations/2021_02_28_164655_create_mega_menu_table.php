e811763ad183aeff754f207898b1bec995cef485
Chris Powell <hosting@ampedwebdesign.co.uk>
2021-07-30 16:11:25 +0100

    Updating migration json -> longtext

--- database/migrations/2021_02_28_164655_create_mega_menu_table.php
+++ database/migrations/2021_02_28_164655_create_mega_menu_table.php
@@ -17,7 +17,7 @@ class CreateMegaMenuTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('description')->nullable();
-            $table->json('menu_json')->after('description')->nullable();
+            $table->longtext('menu_json')->after('description')->nullable();
             $table->timestamps();
         });
     }
